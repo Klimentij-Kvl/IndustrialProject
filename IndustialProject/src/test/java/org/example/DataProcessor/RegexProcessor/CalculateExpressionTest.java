d0d9180388c5656428f5737065c67562a8e9e62d
KeoKisik <drachinskialexey@gmail.com>
2024-11-19 11:00:40 +0300

    initing branch. Class Number

---
+++ IndustialProject/src/test/java/org/example/DataProcessor/RegexProcessor/CalculateExpressionTest.java
@@ -0,0 +1,11 @@
+package org.example.DataProcessor.RegexProcessor;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class CalculateExpressionTest {
+    @Test
+    public void check(){
+        int kek =1;
+        int lol =1;
+        assertEquals(kek,lol);
+    }
+}
