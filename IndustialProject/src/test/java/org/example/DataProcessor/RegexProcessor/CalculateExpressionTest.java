3f2c26837aa21c233624a4bc91c4c6bc639ffb8d
Niki <nikiharoshka@gmail.com>
2024-11-20 15:17:25 +0300

    added Sum class and Mul class

--- IndustialProject/src/test/java/org/example/DataProcessor/RegexProcessor/CalculateExpressionTest.java
+++ IndustialProject/src/test/java/org/example/DataProcessor/RegexProcessor/CalculateExpressionTest.java
@@ -12,10 +12,29 @@ class CalculateExpressionTest {
         Number plusnumber = new Number("+5");
         Number minusnumber = new Number("-5");
         Number keknumber = new Number("5");
+        assertEquals(5, plusnumber.getNumber());
+        assertEquals(-5,minusnumber.getNumber());
+        assertEquals(5,keknumber.getNumber());
 
-        assertEquals("5",plusnumber.CreateNumber());
-        assertEquals("-5",minusnumber.CreateNumber());
-        assertEquals("5",keknumber.CreateNumber());
+        /*Number four = new Number("4");
+        assertEquals("10", four.Plus("6"));*/
     }
 
+    @Test
+    void testSimpleAddition(){
+        Sum twoplusthree = new Sum("2+3");
+        Sum twominusthree = new Sum("2-3");
+        Sum threeminustwo = new Sum("3-2");
+        Sum minustwominusthree = new Sum("-2-3");
+        assertEquals(5,twoplusthree.CalculateSum());
+        assertEquals(-1,twominusthree.CalculateSum());
+        assertEquals(1,threeminustwo.CalculateSum());
+        assertEquals(-5,minustwominusthree.CalculateSum());
+    }
+
+    @Test
+    public void testMultiplication(){
+        Mul twoandthree = new Mul("2*3");
+        assertEquals(6,twoandthree.CalculateMul());
+    }
 }
