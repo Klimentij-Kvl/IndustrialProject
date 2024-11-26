26149b6e20fb33804848bc44700593c39d6cda5e
Corovinus <apancenok@gmail.com>
2024-11-26 20:11:04 +0300

    refactoring ConsoleUI packages

--- src/main/java/org/example/UIProcessor/ConsoleUI/ConsoleUI.java
+++ src/main/java/org/example/UIProcessor/ConsoleUI/ConsoleUI.java
@@ -2,14 +2,6 @@ package org.example.UIProcessor.ConsoleUI;
 
 import org.example.UIProcessor.UIProcessor;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-interface Command {
-    void execute();
-}
-
 public class ConsoleUI implements UIProcessor {
     @Override
     public void start() {
@@ -29,34 +21,3 @@ public class ConsoleUI implements UIProcessor {
 
 }
 
-abstract class Menu {
-    protected Map<Integer, Command> options = new HashMap<>();
-    protected Scanner scanner = new Scanner(System.in);
-
-    public void displayMenu() {
-        System.out.println(getMenuHeader());
-        options.forEach((key, value) -> System.out.println(key + ": " + getOptionName(key)));
-        System.out.println("0: Exit");
-    }
-
-    public void handleInput() {
-        while (true) {
-            displayMenu();
-            System.out.print("Select an option: ");
-            int choice = scanner.nextInt();
-            if (choice == 0) {
-                System.out.println("Exiting...");
-                break;
-            }
-            Command command = options.get(choice);
-            if (command != null) {
-                command.execute();
-            } else {
-                System.out.println("Invalid choice. Please try again.");
-            }
-        }
-    }
-
-    protected abstract String getMenuHeader();
-    protected abstract String getOptionName(int optionKey);
-}
---
+++ src/main/java/org/example/UIProcessor/ConsoleUI/Menus/Menu.java
@@ -0,0 +1,40 @@
+package org.example.UIProcessor.ConsoleUI.Menus;
+
+import org.example.UIProcessor.ConsoleUI.Commands.Command;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+abstract class Menu {
+    protected Map<Integer, Command> options = new HashMap<>();
+    protected Scanner scanner = new Scanner(System.in);
+
+    public void displayMenu() {
+        System.out.println(getMenuHeader());
+        options.forEach((key, value) -> System.out.println(key + ": " + getOptionName(key)));
+        System.out.println("0: Exit");
+    }
+
+    public void handleInput() {
+        while (true) {
+            displayMenu();
+            System.out.print("Select an option: ");
+            int choice = scanner.nextInt();
+            if (choice == 0) {
+                System.out.println("Exiting...");
+                break;
+            }
+            Command command = options.get(choice);
+            if (command != null) {
+                command.execute();
+            } else {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    protected abstract String getMenuHeader();
+
+    protected abstract String getOptionName(int optionKey);
+}
