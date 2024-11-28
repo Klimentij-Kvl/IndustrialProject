12b35e7ffc237ee4c65730f37024c7c1cfae1469
Corovinus <apancenok@gmail.com>
2024-11-28 11:36:30 +0300

    Fix incorrect String input

--- src/main/java/org/example/UIProcessor/ConsoleUI/Menus/Menu.java
+++ src/main/java/org/example/UIProcessor/ConsoleUI/Menus/Menu.java
@@ -3,6 +3,7 @@ package org.example.UIProcessor.ConsoleUI.Menus;
 import org.example.UIProcessor.ConsoleUI.Commands.Command;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -21,20 +22,25 @@ public abstract class Menu {
             System.out.println();
             displayMenu();
             System.out.print("Select an option: ");
-            int choice = scanner.nextInt();
-            if (choice == 0) {
-                System.out.println("Exiting...");
-                break;
+            try {
+                int choice = scanner.nextInt();
+                if (choice == 0) {
+                    System.out.println("Exiting...");
+                    break;
+                }
+                Command command = options.get(choice);
+                if (command != null) {
+                    command.execute();
+                } else {
+                    System.out.println("Invalid choice. Please try again.");
+                }
             }
-            Command command = options.get(choice);
-            if (command != null) {
-                command.execute();
-            } else {
-                System.out.println("Invalid choice. Please try again.");
+            catch (InputMismatchException e) {
+                System.out.println("Invalid choice. Please enter a number between 0 and " + (options.size() + 1));
+                scanner.nextLine();
             }
         }
     }
-
     protected abstract String getMenuHeader();
 
     protected abstract String getOptionName(int optionKey);
