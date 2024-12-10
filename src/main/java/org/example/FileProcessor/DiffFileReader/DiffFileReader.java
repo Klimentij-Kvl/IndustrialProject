af6d5b8dfe072531450dd353582fb4dc8b2232d9
Corovinus <apancenok@gmail.com>
2024-12-10 23:12:18 +0300

    Bug fix

--- src/main/java/org/example/FileProcessor/DiffFileReader/DiffFileReader.java
+++
@@ -1,8 +0,0 @@
-package org.example.FileProcessor.DiffFileReader;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface DiffFileReader {
-    List<String> read(String filePath) throws IOException;
-}
