2fa888cbde055b33a5bc5d1dbddf51e07ce75f47
Klimentij-Kvl <klimentijkvlvsk@gmail.com>
2024-12-22 18:20:36 +0300

    Made tests for DiffFileWriterDecorator

--- src/test/java/org/example/FileProcessor/DiffFileWriterDecoratorTest.java
+++ src/test/java/org/example/FileProcessor/DiffFileWriterDecoratorTest.java
@@ -16,6 +16,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.zip.ZipInputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class DiffFileWriterDecoratorTest {
@@ -35,68 +36,120 @@ public class DiffFileWriterDecoratorTest {
         toWrite.add("seven eight nine");
     }
 
-    @Test
-    void EncTxtWriteTest() throws Exception {
-        File file = new File(PATH_RES + "EncTxtWriteTest.txt");
-        DiffWriter dw = new EncryptionDecorator("12345", new TxtDiffFileWriter(file));
-        dw.write(toWrite);
-        dw.close();
+    List<String> TxtRead(File inFile) throws IOException{
+        List<String> list = new ArrayList<>();
+        try (
+                FileInputStream fis = new FileInputStream(inFile);
+                InputStreamReader isr = new InputStreamReader(fis);
+                BufferedReader br = new BufferedReader(isr)
+        ) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+        }
 
+        return list;
+    }
+
+    void Decrypt(File inFile, File outFile) throws Exception{
         SecretKey secretKey = new SecretKeySpec(Arrays.copyOf("12345".getBytes(), 16), "AES");
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
-        try(FileInputStream fis = new FileInputStream(file);
+        try(FileInputStream fis = new FileInputStream(inFile);
             CipherInputStream cis = new CipherInputStream(fis, cipher);
-            InputStreamReader ir = new InputStreamReader(cis);
-            BufferedReader br = new BufferedReader(ir)
+            FileOutputStream fos = new FileOutputStream(outFile)
         ){
-            String line;
-            while((line = br.readLine()) != null){
-                toRead.add(line);
+            byte[] b = cis.readAllBytes();
+            fos.write(b);
+        }
+    }
+
+    void Dearchive(File inFile, File outFile) throws IOException{
+        try(ZipInputStream zis = new ZipInputStream(new FileInputStream(inFile));
+            FileOutputStream fos = new FileOutputStream(outFile)){
+            zis.getNextEntry();
+            for (int c = zis.read(); c != -1; c = zis.read()) {
+                fos.write(c);
             }
+            fos.flush();
+            zis.closeEntry();
+        }
+    }
+
+    @Test
+    void EncTxtWriteTest() throws Exception {
+        File encFile = new File(PATH_RES + "EncTxtWriteTest.txt");
+        try(DiffWriter dw = new EncryptionDecorator("12345", new TxtDiffFileWriter(encFile))) {
+            dw.write(toWrite);
         }
 
+        File txtFile = new File(PATH_RES + "EncTxtWriteTest2.txt");
+
+        Decrypt(encFile, txtFile);
+        toRead = TxtRead(txtFile);
+
         assertEquals(toWrite, toRead);
-        assertTrue(file.delete());
+        assertTrue(txtFile.delete());
+        assertTrue(encFile.delete());
     }
 
     @Test
     void ZipTxtWriteTest() throws IOException{
-        File file = new File(PATH_RES + "ZipTextWriteTest.txt");
-        DiffWriter dw = new ZipArchivingDecorator(new TxtDiffFileWriter(file));
-        dw.write(toWrite);
-        dw.close();
+        File txtFile = new File(PATH_RES + "ZipTxtWriteTest.txt");
+        try(DiffWriter dw = new ZipArchivingDecorator(new TxtDiffFileWriter(txtFile))) {
+            dw.write(toWrite);
+        }
+
+        File zipFile = new File(PATH_RES + "ZipTxtWriteTest.zip");
+
+        Dearchive(zipFile, txtFile);
+        toRead = TxtRead(txtFile);
+
+        assertTrue(txtFile.delete());
+        assertTrue(zipFile.delete());
+        assertEquals(toWrite, toRead);
     }
 
     @Test
-    void ZipEncTxtWriteTest() throws IOException{
-        File file = new File(PATH_RES + "ZipEncWriteTest.txt");
-        DiffWriter dw = new ZipArchivingDecorator(new EncryptionDecorator("12345", new TxtDiffFileWriter(file)));
-        dw.write(toWrite);
-        dw.close();
+    void ZipEncTxtWriteTest() throws Exception{
+        File txtFile = new File(PATH_RES + "ZipEncWriteTest.txt");
+        try(DiffWriter dw = new ZipArchivingDecorator(new EncryptionDecorator("12345", new TxtDiffFileWriter(txtFile)))) {
+            dw.write(toWrite);
+        }
+
+        File zipEncFile = new File(PATH_RES + "ZipEncWriteTest.zip");
+        File encFile = new File(PATH_RES + "ZipEncWriteTest2.txt");
+
+        Dearchive(zipEncFile, encFile);
+        Decrypt(encFile, txtFile);
+        toRead = TxtRead(txtFile);
+
+        assertEquals(toWrite, toRead);
+        assertTrue(zipEncFile.delete());
+        assertTrue(encFile.delete());
+        assertTrue(txtFile.delete());
     }
 
     @Test
     void EncZipTxtWriteTest() throws Exception{
-        File file = new File(PATH_RES + "EncZipWriteTest.txt");
-        DiffWriter dw = new EncryptionDecorator("12345",
-                                new ZipArchivingDecorator(
-                                new TxtDiffFileWriter(file)));
-        dw.write(toWrite);
-        dw.close();
-
-        SecretKey secretKey = new SecretKeySpec(Arrays.copyOf("12345".getBytes(), 16), "AES");
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-
-        try(FileInputStream fis = new FileInputStream(PATH_RES + "EncZipWriteTest.zip");
-            CipherInputStream cis = new CipherInputStream(fis, cipher);
-            FileOutputStream fos = new FileOutputStream(PATH_RES + "EncZipWriteTest2.zip")
-        ){
-            byte[] b = new byte[4096];
-            while(cis.read(b) != -1)
-                fos.write(b);
+        File txtFile = new File(PATH_RES + "EncZipWriteTest.txt");
+        try(DiffWriter dw = new EncryptionDecorator("12345", new ZipArchivingDecorator(new TxtDiffFileWriter(txtFile))))
+        {
+            dw.write(toWrite);
         }
+
+        File encZipFile = new File(PATH_RES + "EncZipWriteTest.zip");
+        File zipFile = new File(PATH_RES + "EncZipWriteTest2.zip");
+
+        Decrypt(encZipFile, zipFile);
+        Dearchive(zipFile, txtFile);
+        toRead = TxtRead(txtFile);
+
+        assertEquals(toWrite, toRead);
+        assertTrue(encZipFile.delete());
+        assertTrue(zipFile.delete());
+        assertTrue(txtFile.delete());
     }
 }
