a631e9fdba6489bfd750dea71299137df49ec4fd
Klimentij-Kvl <klimentijkvlvsk@gmail.com>
2024-12-31 12:32:10 +0300

    Added reflection method that gets set of classes in package

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,30 @@
 package org.example;
 
+import com.google.common.reflect.ClassPath;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.UIProcessor.GUI.Controller;
+
+import java.io.IOException;
+import java.lang.reflect.Modifier;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello World");
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Industrial Project");
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         launch(args);
     }
 }
--- src/main/java/org/example/UIProcessor/GUI/Controller.java
+++ src/main/java/org/example/UIProcessor/GUI/Controller.java
@@ -1,4 +1,23 @@
 package org.example.UIProcessor.GUI;
 
+import com.google.common.reflect.ClassPath;
+
+import java.io.IOException;
+import java.lang.reflect.Modifier;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Controller {
+    private Set<Class<?>> getConcreteClassesInPackage(String packageName) throws IOException {
+        return ClassPath.from(ClassLoader.getSystemClassLoader())
+                .getAllClasses()
+                .stream()
+                .filter(clazz -> clazz.getPackageName()
+                        .equalsIgnoreCase(packageName))
+                .map(ClassPath.ClassInfo::load)
+                .filter(load -> !Modifier.isAbstract(load.getModifiers()))
+                .collect(Collectors.toSet());
+    }
+
+
 }
