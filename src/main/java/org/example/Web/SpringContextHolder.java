fba4bf8e39531797e5c2dcb3bbc46e36352915db
Corovinus <apancenok@gmail.com>
2025-01-07 20:13:34 +0300

    Added Class for ApplicationContext

---
+++ src/main/java/org/example/Web/SpringContextHolder.java
@@ -0,0 +1,15 @@
+package org.example.Web;
+
+import org.springframework.context.ApplicationContext;
+
+public class SpringContextHolder {
+    private static ApplicationContext context;
+
+    public static void setContext(ApplicationContext ctx) {
+        context = ctx;
+    }
+
+    public static ApplicationContext getContext() {
+        return context;
+    }
+}
